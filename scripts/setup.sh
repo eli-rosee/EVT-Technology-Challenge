96a1a17180cc58a5c6d6b66680ba8329268a4000
eli-rosee <eli.j.rose.04@gmail.com>
2026-04-09 17:49:43 -0500

    added/modified echo statements to start up script to give dynamic feedback with the build / run

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,23 @@
 
 if [ -z "$(docker ps -a -q -f name=awesome-server)" ]; then
     if [[ -f "../certs/server.key" && -f "../certs/server.crt" ]]; then
-        echo "Certs already generated!"
+        echo -e "\nCertificate already exist, skipping generation.\n"
     else
         if ! [ -d "../certs" ]; then
             mkdir ../certs
         fi
-        echo "Certs and / or key do not exist. Generating new private key and self signed certificate."
+        echo -e "\nGenerating new self-signed certificate...\n"
         openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ../certs/server.key -out ../certs/server.crt -config ../configs/openssl.conf
+        echo -e "\nCertificate generated.\n"
     fi
-    
+
+    echo -e "Building Docker image...\n"
     docker build -t localhost-server:1.0 ../
+    echo -e "\nImage built successfully."
+
+    echo -e "Starting container...\n"
     docker run -d -p 80:80 -p 443:443 --name awesome-server localhost-server:1.0
+    echo -e "\nSuccess! Server started: https://localhost\n"
 else
     echo "ERROR. Server Already Running."
 fi
