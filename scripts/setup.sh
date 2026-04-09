d6334044813b3e16c700cc296c5632d79ceb3501
eli-rosee <eli.j.rose.04@gmail.com>
2026-04-09 13:16:16 -0500

    now redirects http requests into https requests

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,5 @@
 
 mkdir certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ../certs/server.key -out ../certs/server.crt -config ../configs/openssl.conf
-docker build -t localhost-server:v3 ../
-docker run -p 443:443 -d localhost-server:v3
+docker build -t localhost-server:1.0 ../
+docker run -p --name awesome-server 80:80 -p 443:443 -d localhost-server:1.0
