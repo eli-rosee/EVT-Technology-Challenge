ed09308c94096d55a3695f364966cdd2bcf4e783
eli-rosee <eli.j.rose.04@gmail.com>
2026-04-09 13:42:55 -0500

    added a check for if the server is already running or not.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ../certs/server.key -out ../certs/server.crt -config ../configs/openssl.conf
-docker build -t localhost-server:1.0 ../
-docker run -p --name awesome-server 80:80 -p 443:443 -d localhost-server:1.0
+if [ -z "$(docker ps -a -q -f name=awesome-server)" ]; then
+    # run your container
+    mkdir certs
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ../certs/server.key -out ../certs/server.crt -config ../configs/openssl.conf
+    docker build -t localhost-server:1.0 ../
+    docker run -d -p 80:80 -p 443:443 --name awesome-server localhost-server:1.0
+else
+    echo "ERROR. Server Already Running."
+fi
