4bf2aa703f6a3b2adbbaab0945134a3349b13df3
eli-rosee <eli.j.rose.04@gmail.com>
2026-04-09 13:56:11 -0500

    added server existence check to logs.sh

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,1 +1,7 @@
-docker logs -f awesome-server
+#!/bin/bash
+
+if [ -z "$(docker ps -a -q -f name=awesome-server)" ]; then
+    echo "ERROR. Server Logs Do Not Exist."
+else
+    docker logs -f awesome-server
+fi
