d68e33ccd2179de1c9e8217d3681298aa10b12f5
eli-rosee <eli.j.rose.04@gmail.com>
2026-04-09 13:03:37 -0500

    created an openssl config for auto generation of cert / key. altered setup.sh to generate the key / cert and automatically build the docker image and run it

--- setup.sh
+++ setup.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
-og_config_path=/etc/nginx/nginx.conf
-new_config_path="$HOME/EVT/EVT-Technology-Challenge/nginx/server.conf"
-
-echo $new_config_path
-echo $og_config_path
-
-echo -e "\nRunning apt update and installing nginx\n"
-
-# Install nginx
-# sudo apt update && sudo apt upgrade -y && sudo apt install nginx -y
-
-echo
-
-# Remove default config or existing symlink
-if [[ -e "$OG_CONFIG_PATH" ]]; then
-    sudo rm "$OG_CONFIG_PATH"
-    echo -e "Removed original nginx config\n"
-fi
-
-# Create symlink to config in repo
-if [[ ! -f "$new_config_path" ]]; then
-    echo -e "ERROR: Config file not found at "$new_config_path"\n"
-    exit 1
-fi
- 
-sudo ln -s "$NEW_CONFIG_PATH" "$OG_CONFIG_PATH"
-echo -e "Symlink created successfully!\n"
-
-# Validate and restart
-sudo nginx -t && sudo systemctl restart nginx
-echo -e "Nginx setup complete!\n"
+mkdir certs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/server.key -out certs/server.crt -config configs/openssl.conf
+docker build -t localhost-server:v3 .
+docker run -p 443:443 -d localhost-server:v3
